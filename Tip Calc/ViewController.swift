561bf69722e3e7de5c1e72ea8a438a5d315a1b08
= <lenardk@uci.edu>
2020-01-03 23:06:48 -0800

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Kaeley Lenard on 1/3/20.
+//  Copyright © 2020 Kaeley Lenard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
